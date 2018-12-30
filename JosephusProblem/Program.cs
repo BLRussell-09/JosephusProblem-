8b3f7431982652390f4950c3025efd9b45a78840
Barry Russell <blrussell09@gmail.com>
2018-12-30 15:48:30 -0600

    problem solves down to 2 ints

--- JosephusProblem/Program.cs
+++ JosephusProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JosephusProblem
 {
@@ -6,7 +7,69 @@ namespace JosephusProblem
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      List<int> numberBay = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+
+      listCrunch(numberBay);
+
+      Console.Read();
     }
+
+    static bool listCrunch(List<int> numberBay)
+    {
+      int dexBump = 0;
+      int dexStart = 1;
+
+      while (numberBay.Count > 2)
+      {
+        var listLength = numberBay.Count;
+        List<int> dropDex = new List<int>();
+
+        dexFindr(dexStart, dexBump, numberBay, dropDex);
+
+        dexCrusher(dropDex, numberBay);
+
+        Console.WriteLine();
+
+        foreach (int number in numberBay)
+        {
+          Console.Write(number + ",");
+        }
+
+        dexBump = listLength % 3 + dexStart;
+        dexStart = 0;
+      }
+
+      return true;
+    }
+
+    static List<int> dexFindr(int dexStart, int dexBump, List<int> numberBay, List<int> dropDex)
+    {
+      var listLength = numberBay.Count;
+      for (int idx = dexStart; idx <= numberBay.Count; idx++)
+      {
+        if (idx % 3 == 0)
+        {
+          if ((idx - 1 + dexBump) < listLength)
+          {
+            dropDex.Add(idx - 1 + dexBump);
+          }
+        }
+      }
+
+      dropDex.Reverse();
+      return dropDex;
+    }
+
+    static List<int> dexCrusher(List<int> dropDex, List<int> numberBay)
+    {
+      foreach (int number in dropDex)
+      {
+        numberBay.RemoveAt(number);
+      } 
+
+      return numberBay;
+    }
+
+
   }
 }
